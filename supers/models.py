06b715b059ebee9d3996000063d2aadcf83659c7
Evan Kopec <ekopec0705@gmail.com>
2022-05-31 10:21:12 -0500

    Created both super_types and supers classes within the models.py files in both apps

--- supers/models.py
+++ supers/models.py
@@ -1,5 +1,6 @@
+from tkinter import CASCADE
 from django.db import models
-
+from super_types.models import SuperType
 # Create your models here.
 
 class Super(models.Model):
@@ -8,4 +9,4 @@ class Super(models.Model):
     primmary_ability = models.CharField(max_length=255)
     secondary_ability = models.CharField(max_length=255)
     catchphrase = models.CharField(max_length=255)
-    super_type = models.ForeignKey()
+    super_type = models.ForeignKey(SuperType, on_delete=models.CASCADE)
