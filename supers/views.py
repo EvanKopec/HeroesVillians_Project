549191f84aa968d87876e6e007f51a0b0e532ce9
Evan Kopec <ekopec0705@gmail.com>
2022-05-31 12:15:37 -0500

    Registered Super and Super_Type Model with admin site

--- supers/views.py
+++ supers/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
-
 from super_types.models import SuperType
 from .serializers import SuperSerializer
 from .models import Super
